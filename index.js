361672781eee12530014897d1995dd6d58f6a43d
Rene Sauve <rdasauve@gmail.com>
2020-10-14 16:29:54 -0700

    aww
    yeee

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const fs = require("fs");
 // const json2xls = require('json2xls');
 const cheerio = require("cheerio");
 const jsonexport = require("jsonexport");
-
+require ('dotenv').config()
 // const path = require("path");
 // const {json2excel, excel2json} = require("js2excel");
 
 // const DxfParser = require("dxf-parser");
 // const pcheerio = require('pseudo-cheerio');
 
-const currentPath = "C:/Users/Student2/OneDrive - Electron Metalworks Ltd/Desktop/svgs/FixThese";
+const currentPath = process.env.CURRENTPATH
 
 
 
@@ -29,40 +29,40 @@ fs.readdir(currentPath, {encoding: "utf8", withFileTypes: true}, function (
               function (err, content) {
                 if (err) reject(err);
                 callback(null, content);
-      
+
               }
             );
           }
-     
+
 
 
           readContent(function (err, content) {
 
             const $ = cheerio.load(content);
             const svg = $("svg");
-            const fileData = 
+            const fileData =
               {
               filename: file.name,
               width: svg.attr("width"),
               height: svg.attr("height"),
             }
-            
-             resolve(([fileData].filter(item => item.width === "210mm")))
-            
+
+             resolve(([fileData].filter(item => item.width != "210mm")))
+
 
           });
-        
+
 
         }).catch((error) => console.log({error}));
       }
- 
+
 
     })
-    
+
   ).then((content) => {
       jsonexport(content, function (err, csv) {
         if (err) return console.error(err);
-        const writer = fs.createWriteStream("filtered-pictures.csv");
+        const writer = fs.createWriteStream("other-filtered-pictures.csv");
 
         writer.write(csv);
       });
